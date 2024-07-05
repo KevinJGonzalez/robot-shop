69f862431c95f3be763b1613c9f193509c57efdb
Kevin Gonzalez <kevinjgonzalez1402@gmail.com>
2024-07-05 16:08:21 -0600

    11° module - organizing application using modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { SiteHeaderComponent } from './site-header/site-header.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SignInComponent } from './user/sign-in/sign-in.component';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [
@@ -20,9 +20,14 @@ import { SignInComponent } from './user/sign-in/sign-in.component';
     SiteHeaderComponent,
     ProductDetailsComponent,
     CartComponent,
-    SignInComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    UserModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
