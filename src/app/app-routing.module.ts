2595f55046035a56e6cf7fa5e89ff9cc7bd1f271
Kevin Gonzalez <kevinjgonzalez1402@gmail.com>
2024-07-05 08:48:27 -0600

    9° module - routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { CatalogComponent } from './catalog/catalog.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'catalog', component: CatalogComponent },
+  { path: 'cart', component: CartComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
